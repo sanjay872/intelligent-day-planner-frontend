fc0b699a6eaf2ee2f1738a879e4e6c506b369e77
Sanjay Sakthivel <sanjaysakthivel2000@gmail.com>
2024-12-30 13:39:59 -0600

    initial project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PlannerComponent } from './planner/planner.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+     path: '',
+     component: HomeComponent,   
+    },
+    {
+        path:'auth',
+        children:[
+            {
+                path:'login',
+                component: LoginComponent,
+            },
+            {
+                path:'register',
+                component: RegisterComponent,
+            }
+        ]
+    },
+    {
+        path:'profile',
+        component:ProfileComponent,
+    },
+    {
+        path:'dashboard',
+        component:DashboardComponent,
+    },
+    {
+        path:'planner',
+        component:PlannerComponent,
+    },
+    {
+        path:'**',
+        redirectTo: '',
+    }
+];
---
+++ src/app/planner/planner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planner',
+  imports: [],
+  templateUrl: './planner.component.html',
+  styleUrl: './planner.component.css'
+})
+export class PlannerComponent {
+
+}
